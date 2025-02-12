9e3b10b70b44ad0d267ecde1b48eab7ce70471f3
zhangchuan <zhangchuan@samsino.com.cn>
2025-02-12 15:45:59 +0800

    ts提示优化

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { App } from 'vue'
 import PreviewFile from './components/PreviewFile.vue'
 
+declare module 'vue' {
+  export interface GlobalComponents {
+    PreviewFile: typeof PreviewFile
+  }
+}
+
+export { PreviewFile }
+
 export default {
   install: (app: App) => {
     app.component('PreviewFile', PreviewFile)
   }
 }
-
-export { PreviewFile }
